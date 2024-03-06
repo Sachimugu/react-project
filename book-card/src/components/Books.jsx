6610cfae073f8f37d7c7d586e43355e2a0edf42b
sachimugu <sachiimugu@gmail.com>
2024-03-06 21:01:46 +0100

    mobile reposivness

--- book-card/src/components/Books.jsx
+++ book-card/src/components/Books.jsx
@@ -5,7 +5,7 @@ import BookCard from "./BookCard";
 const Books = () => {
   return (
     <div className="max-w-5xl m-auto  min-h-screen bg-slate-100">
-      <div className="pt-8 grid grid-cols-3 gap-6 place-items-center">
+      <div className="pt-8 grid sm:grid-cols-3 gap-6 place-items-center">
         {data.map((book, index) => {
           return <BookCard key={index} book={book}></BookCard>;
         })}
