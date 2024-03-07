a8aac8a89575b61199343582f04f3f23200d1710
sachimugu <sachiimugu@gmail.com>
2024-03-07 11:00:46 +0100

    shopping list

---
+++ cart-list/src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { shoppingList } from "./data";
+import { MdDeleteOutline } from "react-icons/md";
+import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
+const App = () => {
+
+  const [data, setData] =useState(shoppingList)
+
+  const handledelete = (id) => {
+
+    const newshoppingList = data.filter((item) => {
+      return item.id != id;
+    });
+    setData(newshoppingList)
+   }
+    
+  
+  return (
+    <div className="max-w-5xl  m-auto py-4">
+      {data.map((item) => {
+        return (
+          <ItemCard
+            key={item.price}
+            item={item}
+            handledelete={handledelete}
+          ></ItemCard>
+        );
+      })}
+    </div>
+  );
+};
+
+const ItemCard = (props) => {
+  const { item, price, id } = props.item;
+  const handledelete = props.handledelete
+
+  
+  return (
+    <>
+      <div className="grid place-items-center  grid-cols-3 p-4 bg-blue-100 my-4 rounded-md">
+        <div>{item}</div>
+        <div>{price}</div>
+
+        <div className="">
+          <button onClick={()=>handledelete(id)} className="text-red-400 hover:text-red-800">
+            <MdDeleteOutline />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ cart-list/src/data.jsx
@@ -0,0 +1,7 @@
+export const shoppingList = [
+  { item: "Apples", price: 2.99, quantity: 3, id: 1 },
+  { item: "Milk", price: 1.99, quantity: 1, id: 2 },
+  { item: "Bread", price: 2.49, quantity: 2, id: 3 },
+  { item: "Eggs", price: 3.49, quantity: 1, id: 4 },
+  { item: "Chicken", price: 5.99, quantity: 2, id: 5 },
+];
